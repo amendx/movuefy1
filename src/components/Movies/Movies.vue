4fcdce691385375e739779e17fbe86c1e9dd4bde
amendx <amacve@gmail.com>
2019-03-27 18:50:09 -0300

    :elephant: -> Pagination alignment

--- src/components/Movies/Movies.vue
+++ src/components/Movies/Movies.vue
@@ -1,52 +1,70 @@
 
 <template>
-  <v-container>
-    <vue-instant
-      name="customName"
-      type="google"
-      placeholder="Search Movie..."
-      :show-autocomplete="true"
-      :autofocus="false"
-      :suggestions="suggestions"
-      :suggestion-attribute="suggestionAttribute"
-      v-model="searchterm"
-      @selected="selected"
-      @input="changed"
-      @enter="search"
-    ></vue-instant>
-    <template v-if="result"></template>
-    <v-layout row wrap v-for="movie in movies" :key="movie.id" class="mb-2">
-      <v-flex xs12 sm10 md8 lg6 offset-sm1 offset-md2 offset-lg3>
-        <v-card class="card">
-          <v-container fluid>
-            <v-layout row justify-start>
-              <v-flex xs12 sm6 md9>
-                <v-card-media
-                  class="card-image"
-                  :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
-                  height="70vh"
-                ></v-card-media>
-              </v-flex>
-              <v-flex xs12 sm6 md9>
-                <v-card-title primary-title>
-                  <div>
-                    <h5 class="white--text mb-0">{{ movie.title }}</h5>
-                    <div>{{ movie.overview }}</div>
-                  </div>
-                </v-card-title>
-                <v-card-actions>
-                  <v-btn flat :to="'/movies/' + movie.id">
-                    <v-icon left light>arrow_forward</v-icon>View Movie
-                  </v-btn>
-                </v-card-actions>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card>
-      </v-flex>
-    </v-layout>
-    <v-pagination @input="next" :value="page" :length="7" :total-visible="7"></v-pagination>
-  </v-container>
+  <div>
+    <v-container>
+      <v-layout align-center justify-center>
+        <v-flex offset-md3 offset-lg3>
+          <vue-instant
+            class="search-movie"
+            name="search-movie"
+            type="google"
+            placeholder="Search Movie..."
+            :show-autocomplete="true"
+            :autofocus="false"
+            :suggestions="suggestions"
+            :suggestion-attribute="suggestionAttribute"
+            v-model="searchterm"
+            @selected="selected"
+            @input="changed"
+            @enter="search"
+            v-bind:style="{ width: '49vh'}"
+          ></vue-instant>
+          <v-spacer></v-spacer>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-container>
+      <!-- <template v-if="result"></template> -->
+      <v-layout row wrap v-for="movie in movies" :key="movie.id" class="mb-2">
+        <v-flex xs12 sm10 md8 lg6 offset-sm1 offset-md2 offset-lg3>
+          <v-card class="card">
+            <v-container fluid>
+              <v-layout row justify-start>
+                <v-flex xs12 sm6 md9>
+                  <v-card-media
+                    class="card-image"
+                    :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+                    height="70vh"
+                  ></v-card-media>
+                </v-flex>
+                <v-flex xs12 sm6 md9>
+                  <v-card-title primary-title>
+                    <div>
+                      <h5 class="white--text mb-0">{{ movie.title }}</h5>
+                      <div>{{ movie.overview }}</div>
+                    </div>
+                  </v-card-title>
+                  <v-card-actions>
+                    <v-btn flat :to="'/movies/' + movie.id">
+                      <v-icon left light>arrow_forward</v-icon>View Movie
+                    </v-btn>
+                  </v-card-actions>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container>
+      <v-layout justify-center>
+        <v-flex xs12 sm6 md9>
+          <v-pagination @input="next" :value="page" :total-visible="5" :length="5"></v-pagination>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 
@@ -101,9 +119,7 @@ export default {
     selected: function(current) {
       this.selection = current;
     },
-    changed: function() {
-      console.log("changed");
-    },
+    changed: function() {},
     search: function() {}
   },
   created() {
@@ -114,6 +130,10 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+.searchbox
+ .sbx-google
+    width: 49vh;
+
 
 .card-image 
   transform: scale(0.7, 0.7);
